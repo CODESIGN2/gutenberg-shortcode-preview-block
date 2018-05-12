793b801ebf4c509144b0596283749da8f07135b7
Lewis Cowles <lewiscowles@me.com>
2018-05-12 23:53:21 +0100

    Updated build.sh
    Updated build.sh so that it would upload into WP, miss the .git folder and the media folder (which are not needed at this time)
    
    Signed-off-by: Lewis Cowles <lewiscowles@me.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-zip -r shortcode-block.zip ../shortcode-block -x "*node_modules*" -x "*package.json" -x "*preview.js" -x "*block.js" -x "*sandbox-custom.js" -x "*index.js" -x "*.babelrc" -x "*.zip"
+cd ../
+zip -r shortcode-block.zip shortcode-block -x "*node_modules*" -x "*package.json" -x "*preview.js" -x "*block.js" -x "*sandbox-custom.js" -x "*index.js" -x "*.babelrc" -x "*.zip" -x "*.git*" -x "*media*"
 
