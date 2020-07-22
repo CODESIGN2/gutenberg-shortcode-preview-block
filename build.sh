c4b7927e0e352e36708c9fc0f6bda0931bc18f1c
Lewis Cowles <lewiscowles@me.com>
2020-07-22 13:29:51 +0100

    Update BuildRelease Script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -f ../shortcode-block*.zip
-zip -r ../shortcode-block$1.zip . -x "*node_modules*" -x "*webpack.config.js" -x "*package-lock.json" -x "*package.json" -x "*preview.js" -x "*block.js" -x "*sandbox-custom.js" -x "*index.js" -x "*.babelrc" -x "*.zip" -x "*.git*" -x "*media*"
+zip -r ../shortcode-block$1.zip . -x "*block.*.json*" -x "*storybook*" -x "*node_modules*" -x "*webpack.config.js" -x "*package-lock.json" -x "*package.json" -x "*preview.js" -x "*block.js" -x "*sandbox-custom.js" -x "*index.js" -x "*.babelrc" -x "*.zip" -x "*.git*" -x "*media*"
