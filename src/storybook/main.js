a05df3a7c317914b1267b4231fc53b9d67d05fda
Lewis Cowles <lewiscowles@me.com>
2020-07-22 04:59:17 +0100

    Initial setup for Storybook

---
+++ src/storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    {
+			name: '@storybook/addon-docs',
+			options: { configureJSX: true },
+		},
+		'@storybook/addon-knobs',
+		'@storybook/addon-storysource',
+		'@storybook/addon-viewport',
+		'@storybook/addon-a11y',
+  ],
+  
+};
