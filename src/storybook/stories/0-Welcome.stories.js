a58232d63bd66403d9f27ad8bedda80c42450c04
Lewis Cowles <lewiscowles@me.com>
2020-07-22 13:24:53 +0100

    Edited Media and Initial Story

--- src/storybook/stories/0-Welcome.stories.js
+++ src/storybook/stories/0-Welcome.stories.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { linkTo } from '@storybook/addon-links';
-import { Welcome } from '@storybook/react/demo';
 
 export default {
   title: 'Welcome',
-  component: Welcome,
 };
 
-export const ToStorybook = () => <Welcome showApp={linkTo('button')} />;
+import CD2BlockEditImg from '../../media/cd2-block-edit.png';
+import CD2BlockPreviewImg from '../../media/cd2-block-preview.png';
 
-ToStorybook.story = {
-  name: 'to Storybook',
-};
+export const ToStorybook = () => <div>
+  <h1>Welcome</h1>
+  <p>This is the Storybook for the CD2 Shortcode Preview Gutenberg Block.</p>
+  <p>You can use the side-bar to visit <a href="http://localhost:6006/?path=/story/shortcode-preview-block--default">Shortcode Preview Block</a>.</p>
+  <p>I have not yet worked out how to have the component as the only block, so you'll see a number of Core Gutenberg blocks.</p>
+  <p>It's also a little different to the WordPress editor, so YMMV and bugs may come up. This is tracking a Gutenberg project effort to use Storybook as an alternative to standing up a full WordPress installation. Because of the way this plugin works requiring both WordPress and it's API, you'll notice globals used, and use of a Mocking tool for requests, rather than real Shortcode rendering.</p>
+  <p>Thanks for your interest.</p>
+  <figure>
+    <img src={CD2BlockEditImg} alt={""} />
+    <figcaption>Editing Shortcode technical content and initial input view.</figcaption>
+  </figure>
+  <figure>
+    <img src={CD2BlockPreviewImg} alt={""} />
+    <figcaption>Preview of Shortcode technical content presentation view.</figcaption>
+  </figure>
+</div>;
