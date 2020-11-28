c1807f84a4cbcd0a5cb176cf6180a0548f315b37
Lewis Cowles <lewiscowles@me.com>
2020-11-28 10:19:04 +0000

    Interim step: build scripts
    Copy JS build assets into src for-now

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+@echo off
 rm -f ../shortcode-block.zip
+copy ../build/block.* ./
 7z a -tzip -r ../shortcode-block.zip . -x!"*block.*.json*" -x!"*storybook*" -x!"*node_modules*" -x!"*webpack.config.js" -x!"*package-lock.json" -x!"*package.json" -x!"*preview.js" -x!"*block.js" -x!"*sandbox-custom.js" -x!"*index.js" -x!"*.babelrc" -x!"*.zip" -x!"*.git*" -x!"*media*"
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rm -f ../shortcode-block*.zip
+cp ../build/block.* ./
 zip -r ../shortcode-block$1.zip . -x "*block.*.json*" -x "*storybook*" -x "*node_modules*" -x "*webpack.config.js" -x "*package-lock.json" -x "*package.json" -x "*preview.js" -x "*block.js" -x "*sandbox-custom.js" -x "*index.js" -x "*.babelrc" -x "*.zip" -x "*.git*" -x "*media*"
