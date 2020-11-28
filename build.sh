6d4c707e6b920b6773ed0497dea7f88b5c719649
Lewis Cowles <lewiscowles@me.com>
2020-11-28 22:04:43 +0000

    Updated build script(s)

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -f ../shortcode-block*.zip
 cp ../build/block.* ./
+cp ../build/readme.txt ./
 zip -r ../shortcode-block$1.zip . -x "*block.*.json*" -x "*storybook*" -x "*node_modules*" -x "*webpack.config.js" -x "*package-lock.json" -x "*package.json" -x "*preview.js" -x "*block.js" -x "*sandbox-custom.js" -x "*index.js" -x "*.babelrc" -x "*.zip" -x "*.git*" -x "*media*"
